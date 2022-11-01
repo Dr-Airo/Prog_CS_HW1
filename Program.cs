55e566a4a1d15a42f170b6a1c7f3cbef1caf89d2
AyratN <nurtdinov_ar@mail.ru>
2022-11-01 22:39:05 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// Console.WriteLine("Введите первое натуральное число"); // Принимаем 2 int числа и показываем, какое большее, а какое меньшее
+// int x=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе натуральное число");
+// int y=Convert.ToInt32(Console.ReadLine());
+// if (x>y) Console.WriteLine($"Число {x} - большее, а {y} -  меньшее");
+// else Console.WriteLine($"Число {y} - большее, а {x} -  меньшее");
+
+// Console.WriteLine("Введите первое натуральное число"); // Находим максимальное из 3 введеных чисел
+// int x=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе натуральное число");
+// int y=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье натуральное число");
+// int z=Convert.ToInt32(Console.ReadLine());
+// int max = x;
+// if (y > max) max = y;
+// if (z > max) max = z;
+// Console.WriteLine($"{max} - максимальное число");
+
+// Console.WriteLine("Введите натуральное число"); // Показать четные числа от 1 до N
+// int x=Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// while (count <=x)
+// {
+//     if (count % 2 == 0)
+//     {
+//         Console.Write(count+", ");
+//         count++;
+//     }
+//     else count++;
+// }
+
+// Console.WriteLine("Введите натуральное число"); // Число четное (true/false)
+// int x=Convert.ToInt32(Console.ReadLine());
+// if (x % 2 == 0) Console.WriteLine("Вы ввели чётное число");
+// else Console.WriteLine("Вы ввели нечётное число");
